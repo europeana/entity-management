80b19e1d03de9a65fb5544359d02cb923a121303
Immanuel Kattey <immanuel.kattey@europeana.eu>
2021-02-28 21:57:03 +0100

    Make EntityRecordTest a proper integration test

--- entity-management-web/src/integration-test/resources/mongo-docker/init-mongo.sh
+++ entity-management-web/src/integration-test/resources/mongo-docker/init-mongo.sh
@@ -1,30 +1,20 @@
 # This script creates a mongo user, the entity database and the Spring Batch
 # JobRepository
 mongo -- "$MONGO_INITDB_DATABASE" <<EOF
-#    if [[ -z ${MONGO_INITDB_ROOT_USERNAME:-} || -z ${MONGO_INITDB_ROOT_PASSWORD:-} ]]; then
-#        echo "Mongo admin user or password missing. Cannot create initial user and databases"
-#        exit
-#    fi
-
     var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
     var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
 
 
     db.auth(rootUser, rootPassword);
 
-#if [[ -n ${EM_DB_USER:-} && -n ${EM_DB_PASSWORD:-} ]]; then
-
-    var user = '$EM_DB_USER';
-    var passwd = '$EM_DB_PASSWORD';
     db.createUser({
-        user: 'api_user',
-        pwd: 'password',
+        user: '$EM_DB_USER',
+        pwd: '$EM_DB_PASSWORD',
         roles: [
             { role: 'readWrite', db: '$EM_APP_DB' },
             { role: 'readWrite', db: '$EM_BATCH_DB' }
             ],
     });
-#fi
 
 
 db.getSiblingDB('$EM_APP_DB').createCollection('temp');
