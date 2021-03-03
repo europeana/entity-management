8795fbf1abc51ed426a1b6d4a1addf4a2abe4357
StevaneticS <StevaneticS@N3DSS1905.D03.arc.local>
2021-03-03 13:23:51 +0100

    added test files for the entities in both .xml and .json formats

--- entity-management-web/src/test/java/eu/europeana/entitymanagement/web/service/impl/MetisDereferenceServiceTest.java
+++ entity-management-web/src/test/java/eu/europeana/entitymanagement/web/service/impl/MetisDereferenceServiceTest.java
@@ -3,16 +3,23 @@ package eu.europeana.entitymanagement.web.service.impl;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.io.FileWriter;
+
 import javax.annotation.Resource;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import eu.europeana.api.commons.error.EuropeanaApiException;
 import eu.europeana.entitymanagement.EntityManagementApp;
 import eu.europeana.entitymanagement.config.AppConfig;
+import eu.europeana.entitymanagement.definitions.model.Agent;
 import eu.europeana.entitymanagement.definitions.model.Concept;
+import eu.europeana.entitymanagement.exception.HttpBadRequestException;
 
 /**
  * JUnit test for testing the EMControllerTest class
@@ -53,6 +60,63 @@ public class MetisDereferenceServiceTest {
 
 	
     }
-  
+
+    @Test
+    public void dereferenceEntitiesById() throws Exception {
+    	
+		//Agent
+    	String XML_FILE_AGENT = "src\\test\\resources\\metis-deref\\agent.xml";
+		String agentId = "http://www.wikidata.org/entity/Q762";
+		String metisResponseBodyAgent = metisDerefService.getMetisWebClient().get()
+				.uri(uriBuilder -> uriBuilder.path("/dereference").queryParam("uri", agentId).build())
+				.accept(MediaType.APPLICATION_XML).retrieve()
+				// return 400 for 4xx responses from Metis
+				.onStatus(HttpStatus::is4xxClientError,
+					response -> response.bodyToMono(String.class).map(HttpBadRequestException::new))
+				// return 500 for everything else
+				.onStatus(HttpStatus::isError,
+					response -> response.bodyToMono(String.class).map(EuropeanaApiException::new))
+				.bodyToMono(String.class).block();
+
+		FileWriter fw_agent = new FileWriter(XML_FILE_AGENT);
+		fw_agent.append(metisResponseBodyAgent);
+		fw_agent.close();
+	    
+	    //Place
+    	String XML_FILE_PLACE = "src\\test\\resources\\metis-deref\\place.xml";
+		String placeId = "https://sws.geonames.org/2988507/";
+		String metisResponseBodyPlace = metisDerefService.getMetisWebClient().get()
+				.uri(uriBuilder -> uriBuilder.path("/dereference").queryParam("uri", placeId).build())
+				.accept(MediaType.APPLICATION_XML).retrieve()
+				// return 400 for 4xx responses from Metis
+				.onStatus(HttpStatus::is4xxClientError,
+					response -> response.bodyToMono(String.class).map(HttpBadRequestException::new))
+				// return 500 for everything else
+				.onStatus(HttpStatus::isError,
+					response -> response.bodyToMono(String.class).map(EuropeanaApiException::new))
+				.bodyToMono(String.class).block();
+
+		FileWriter fw_place = new FileWriter(XML_FILE_PLACE);
+		fw_place.write(metisResponseBodyPlace);
+		fw_place.close();
+	    
+	    //Concept
+    	String XML_FILE_CONCEPT = "src\\test\\resources\\metis-deref\\concept.xml";
+		String conceptId = "http://www.wikidata.org/entity/Q152095";
+		String metisResponseBodyConcept = metisDerefService.getMetisWebClient().get()
+				.uri(uriBuilder -> uriBuilder.path("/dereference").queryParam("uri", conceptId).build())
+				.accept(MediaType.APPLICATION_XML).retrieve()
+				// return 400 for 4xx responses from Metis
+				.onStatus(HttpStatus::is4xxClientError,
+					response -> response.bodyToMono(String.class).map(HttpBadRequestException::new))
+				// return 500 for everything else
+				.onStatus(HttpStatus::isError,
+					response -> response.bodyToMono(String.class).map(EuropeanaApiException::new))
+				.bodyToMono(String.class).block();
+
+		FileWriter fw_concept = new FileWriter(XML_FILE_CONCEPT);
+		fw_concept.write(metisResponseBodyConcept);
+		fw_concept.close();	    
+    }
   
 }
