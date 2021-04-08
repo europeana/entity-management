d8a1b639400b822f7a737b9b0d47b5141960b50e
Immanuel Kattey <immanuel.kattey@europeana.eu>
2021-04-08 17:46:42 +0100

    EA-2519: Add missing file

--- entity-management-utils/src/main/java/eu/europeana/entitymanagement/serialization/mixins/XmlIgnoreFieldsMixin.java
+++ entity-management-utils/src/main/java/eu/europeana/entitymanagement/serialization/mixins/XmlIgnoreFieldsMixin.java
@@ -1,4 +1,4 @@
-package eu.europeana.entitymanagement.serialization;
+package eu.europeana.entitymanagement.serialization.mixins;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -7,7 +7,7 @@ import javax.xml.bind.annotation.XmlTransient;
 /**
  * Ignores the context field during serialization
  */
-public abstract class EntityContextIgnoreMixin {
+public abstract class XmlIgnoreFieldsMixin {
   @JsonIgnore
   public abstract String getContext();
 }
