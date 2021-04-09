c207d5b539adba3e6056874359987a828020b776
Immanuel Kattey <immanuel.kattey@europeana.eu>
2021-04-09 19:57:45 +0100

    Fix issue with SOCKS proxy

--- entity-management-web/src/main/java/eu/europeana/entitymanagement/EntityManagementApp.java
+++ entity-management-web/src/main/java/eu/europeana/entitymanagement/EntityManagementApp.java
@@ -1,5 +1,7 @@
 package eu.europeana.entitymanagement;
 
+import eu.europeana.entitymanagement.common.config.SocksProxyConfig;
+import eu.europeana.entitymanagement.config.SocksProxyActivator;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
@@ -26,17 +28,16 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @EnableBatchProcessing
 public class EntityManagementApp extends SpringBootServletInitializer {
 
-    /**
-     * Main entry point of this application
-     * @param args command-line arguments
-     */
     public static void main(String[] args) {
         // When deploying to Cloud Foundry, this will log the instance index number, IP and GUID
         LogManager.getLogger(EntityManagementApp.class).
-                info("CF_INSTANCE_INDEX  = {}, CF_INSTANCE_GUID = {}, CF_INSTANCE_IP  = {}",
-                    System.getenv("CF_INSTANCE_INDEX"),
-                    System.getenv("CF_INSTANCE_GUID"),
-                    System.getenv("CF_INSTANCE_IP"));
+            info("CF_INSTANCE_INDEX  = {}, CF_INSTANCE_GUID = {}, CF_INSTANCE_IP  = {}",
+                System.getenv("CF_INSTANCE_INDEX"),
+                System.getenv("CF_INSTANCE_GUID"),
+                System.getenv("CF_INSTANCE_IP"));
+
+        // Activate socks proxy (if your application requires it)
+        SocksProxyActivator.activate(new SocksProxyConfig("entitymanagement.properties", "entitymanagement.user.properties"));
 
         SpringApplication.run(EntityManagementApp.class, args);
     }
