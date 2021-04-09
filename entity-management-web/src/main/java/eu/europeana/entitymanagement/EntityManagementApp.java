190455d4439516fdd264825c6a7fe9029146162b
Immanuel <ikattey@users.noreply.github.com>
2021-04-09 20:44:38 +0100

    Fix issue with SOCKS proxy (#55)

--- entity-management-web/src/main/java/eu/europeana/entitymanagement/EntityManagementApp.java
+++ entity-management-web/src/main/java/eu/europeana/entitymanagement/EntityManagementApp.java
@@ -1,5 +1,7 @@
 package eu.europeana.entitymanagement;
 
+import eu.europeana.entitymanagement.common.config.SocksProxyConfig;
+import eu.europeana.entitymanagement.config.SocksProxyActivator;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
@@ -37,6 +39,9 @@ public class EntityManagementApp extends SpringBootServletInitializer {
                     System.getenv("CF_INSTANCE_INDEX"),
                     System.getenv("CF_INSTANCE_GUID"),
                     System.getenv("CF_INSTANCE_IP"));
+
+        // Activate socks proxy (if your application requires it)
+        SocksProxyActivator.activate(new SocksProxyConfig("entitymanagement.properties", "entitymanagement.user.properties"));
 
         SpringApplication.run(EntityManagementApp.class, args);
     }
