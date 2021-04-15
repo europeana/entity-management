a24e8bde7d40c4a6c9c2a73c0f6e509f4f65b6e8
GordeaS <GordeaS@N3DSS1923.D03.arc.local>
2021-04-15 11:14:13 +0200

    #EA2470 refactor and enhance LanguageCodes parsing and validation

---
+++ entity-management-web/src/test/java/eu/europeana/entitymanagement/config/LanguageCodesTest.java
@@ -0,0 +1,48 @@
+package eu.europeana.entitymanagement.config;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import eu.europeana.entitymanagement.common.config.AppConfigConstants;
+import eu.europeana.entitymanagement.common.config.LanguageCodes;
+
+/**
+ * JUnit test to check if DataSources are properly deserialized from XML
+ */
+@SpringBootTest()
+public class LanguageCodesTest {
+
+    @Autowired
+    @Qualifier(AppConfigConstants.BEAN_XML_MAPPER)
+    private XmlMapper xmlMapper;
+
+
+    @Test
+    public void shouldDeserializeLanugageCodes() throws IOException {
+	String LANGUAGECODES_XML = "/languagecodes/test-languagecodes.xml";
+        InputStream is = getClass().getResourceAsStream(LANGUAGECODES_XML);
+
+        LanguageCodes languageCodes = xmlMapper.readValue(is, LanguageCodes.class);
+        assertNotNull(languageCodes);
+        assertNotNull(languageCodes.getLanguages());
+        assertNotNull(languageCodes.getAltLangMap());
+        assertEquals(24, languageCodes.getLanguages().size());
+        assertEquals(54, languageCodes.getAltLangMap().size());
+        assertTrue(languageCodes.isValidLanguageCode("en"));
+        assertEquals("en", languageCodes.getByAlternativeCode("en-gb"));
+        assertEquals("en", languageCodes.getByAlternativeCode("eng"));
+        
+    }
+
+}
