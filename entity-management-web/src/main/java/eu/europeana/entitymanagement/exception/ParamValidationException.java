889ade07fecd2da02d4d86c95221416e6240d2b4
Immanuel Kattey <immanuel.kattey@europeana.eu>
2021-05-27 13:16:57 +0100

    EA-2325: Make 'type' mandatory in entity registration request body

--- entity-management-web/src/main/java/eu/europeana/entitymanagement/exception/ParamValidationException.java
+++ entity-management-web/src/main/java/eu/europeana/entitymanagement/exception/ParamValidationException.java
@@ -1,40 +1,30 @@
 package eu.europeana.entitymanagement.exception;
 
+import eu.europeana.api.commons.error.EuropeanaApiException;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 
 import eu.europeana.api.commons.web.exception.HttpException;
 import eu.europeana.entitymanagement.common.config.I18nConstants;
 
+public class ParamValidationException extends EuropeanaApiException {
 
-public class ParamValidationException extends HttpException{
+	public ParamValidationException(String msg) {
+		super(msg);
+	}
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 3664526076494279093L;
-//	public static final String MESSAGE_BLANK_PARAMETER_VALUE = "Invalid request. Parameter value must not be null or empty!";
-	
-//	String parameterName;
-//	String parameterValue;
-	
-	
-	public ParamValidationException(String parameterName, String parameterValue){
-		this(I18nConstants.INVALID_PARAM_VALUE, parameterName, parameterValue, null);
+	@Override
+	public boolean doLog() {
+		return false;
 	}
-	
-	public ParamValidationException(String i18nKey, String parameterName, String parameterValue){
-		this(i18nKey, parameterName, parameterValue, null);
+
+	@Override
+	public boolean doLogStacktrace() {
+		return false;
 	}
-	public ParamValidationException(String i18nKey, String parameterName, String parameterValue, Throwable th){
-		this(i18nKey, parameterName, parameterValue, HttpStatus.BAD_REQUEST, th);
-	}
-	
-	public ParamValidationException(String i18nKey, String parameterName, String parameterValue, HttpStatus status, Throwable th){
-		this(i18nKey, i18nKey, new String[]{parameterName, parameterValue}, status, th);
-	}
-	
-	public ParamValidationException(String message, String i18nKey, String[] i18nParams, HttpStatus status, Throwable th){
-		super(message + " " + StringUtils.join(i18nParams, ':'), i18nKey, i18nParams, status, th);
+
+	@Override
+	public HttpStatus getResponseStatus() {
+		return HttpStatus.BAD_REQUEST;
 	}
 }
