359253c53b960762da317282d6576f1a346492e6
Immanuel Kattey <ikattey@users.noreply.github.com>
2021-07-08 11:43:40 +0100

    EA-2626: Make Europeana provenance configurable (#99)
    Co-authored-by: Srdjan Stevanetic <srdjan.stevanetic@gmail.com>

--- entity-management-common/src/main/java/eu/europeana/entitymanagement/common/config/DataSources.java
+++ entity-management-common/src/main/java/eu/europeana/entitymanagement/common/config/DataSources.java
@@ -9,7 +9,9 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
 @JacksonXmlRootElement(localName = "config")
 public class DataSources {
-	
+
+	public final static String EUROPEANA_ID = "europeana";
+
 	@JacksonXmlElementWrapper(useWrapping = false)
 	@JacksonXmlProperty(localName = "source")
 	private List<DataSource> datasources;
@@ -21,21 +23,25 @@ public class DataSources {
 	/**
 	 * Checks if a datasource is configured for the given entity ID
 	 *
-	 * @param id id to match
+	 * @param url url to match
 	 * @return true if a Datasource match is configured, false otherwise.
 	 */
-	public boolean hasDataSource(String id) {
-		return datasources.stream().anyMatch(s -> id.contains(s.getUrl()));
+	public boolean hasDataSource(String url) {
+		return datasources.stream().anyMatch(s -> url.contains(s.getUrl()));
 	}
 
 	/**
 	 * Gets the configured data source for the given entity ID
 	 *
-	 * @param id entity ID
+	 * @param url entity ID
 	 * @return Matching data source, or empty Optional if none found
 	 */
-	public Optional<DataSource> getDatasource(String id) {
-		return datasources.stream().filter(s -> id.contains(s.getUrl())).findFirst();
+	public Optional<DataSource> getDatasource(String url) {
+		return datasources.stream().filter(s -> url.contains(s.getUrl())).findFirst();
+	}
+
+	public Optional<DataSource> getEuropeanaDatasource(){
+		return datasources.stream().filter(s -> EUROPEANA_ID.equals(s.getId())).findFirst();
 	}
 
 }
