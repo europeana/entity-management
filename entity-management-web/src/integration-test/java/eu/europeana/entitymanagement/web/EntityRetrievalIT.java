30158f6994d5dd0b11d357e8155b488dd3a012e2
Immanuel Kattey <ikattey@users.noreply.github.com>
2021-09-09 10:19:31 +0200

    EA-2631: Add standalone tests for response headers (#118)

--- entity-management-web/src/integration-test/java/eu/europeana/entitymanagement/web/EntityRetrievalIT.java
+++ entity-management-web/src/integration-test/java/eu/europeana/entitymanagement/web/EntityRetrievalIT.java
@@ -1,23 +1,26 @@
 package eu.europeana.entitymanagement.web;
 
-import eu.europeana.entitymanagement.definitions.model.Agent;
-import eu.europeana.entitymanagement.definitions.model.EntityRecord;
-import eu.europeana.entitymanagement.vocabulary.EntityTypes;
-import eu.europeana.entitymanagement.vocabulary.WebEntityConstants;
-import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.ResultActions;
-
 import static eu.europeana.entitymanagement.testutils.BaseMvcTestUtils.*;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.any;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import eu.europeana.entitymanagement.definitions.model.EntityRecord;
+import eu.europeana.entitymanagement.vocabulary.EntityTypes;
+import eu.europeana.entitymanagement.vocabulary.WebEntityConstants;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
 
-
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EntityRetrievalIT extends BaseWebControllerTest {
 
     @Test
@@ -130,7 +133,7 @@ public class EntityRetrievalIT extends BaseWebControllerTest {
         		.andExpect(jsonPath("$.description").isNotEmpty())
         		.andExpect(jsonPath("$.alternateName").isNotEmpty());
     }
-    
+
     @Test
     public void retrieveAgentExternalSchemaOrgShouldBeSuccessful() throws Exception {
 
@@ -145,11 +148,9 @@ public class EntityRetrievalIT extends BaseWebControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.@id", is(entityRecord.getEntityId())));
 
-        checkAllowHeaderForGET(resultActions);
-
         for (String sameAsElem : entityRecord.getEntity().getSameAs()) {
         	resultActions.andExpect(jsonPath("$.sameAs", Matchers.hasItem(sameAsElem)));
-        }        
+        }
         resultActions.andExpect(jsonPath("$.gender").isNotEmpty());
         resultActions.andExpect(jsonPath("$.deathDate").isNotEmpty());
         resultActions.andExpect(jsonPath("$.birthDate").isNotEmpty());
