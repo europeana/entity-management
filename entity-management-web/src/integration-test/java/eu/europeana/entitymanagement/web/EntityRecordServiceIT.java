1c301f5b5179980a8dd1729e2bcc4a62dc4d33e0
GordeaS <GordeaS@N3DSS1923.D03.arc.local>
2021-10-07 15:50:52 +0200

    #EA-2754 updated test to use the new version of Metis response, fix for expected outcome

--- entity-management-web/src/integration-test/java/eu/europeana/entitymanagement/web/EntityRecordServiceIT.java
+++ entity-management-web/src/integration-test/java/eu/europeana/entitymanagement/web/EntityRecordServiceIT.java
@@ -234,6 +234,7 @@ public class EntityRecordServiceIT extends AbstractIntegrationTest{
 
         entityRecordService.dropRepository();
         // create record for agent-davinci-referential-integrity.json
+        //TODO: change the input data to use the wikidata URIs instead of semium time
         Agent agentDaVinci = objectMapper.readValue(loadFile(AGENT_DA_VINCI_REFERENTIAL_INTEGRITY_JSON), Agent.class);
         EntityRecord entityRecord1 = new EntityRecord();
         entityRecord1.setEntity(agentDaVinci);
@@ -292,16 +293,18 @@ public class EntityRecordServiceIT extends AbstractIntegrationTest{
 
         metisResponse = TIMESPAN_15_REFERENTIAL_INTEGRTITY;
         entityFromMetisResponse = getMetisResponse(metisResponse).toEntityModel();
+        entityFromMetisResponse.getSameReferenceLinks().add(entityFromMetisResponse.getEntityId());
         EntityRecord entityRecord9 = new EntityRecord();
         entityRecord9.setEntity(entityFromMetisResponse);
-        entityRecord9.setEntityId(entityFromMetisResponse.getEntityId());
+        entityRecord9.setEntityId("http://data.europeana.eu/timespan/15");
         entityRecordService.saveEntityRecord(entityRecord9);
 
         metisResponse = TIMESPAN_16_REFERENTIAL_INTEGRTITY;
         entityFromMetisResponse = getMetisResponse(metisResponse).toEntityModel();
+        entityFromMetisResponse.getSameReferenceLinks().add(entityFromMetisResponse.getEntityId());
         EntityRecord entityRecord10 = new EntityRecord();
         entityRecord10.setEntity(entityFromMetisResponse);
-        entityRecord10.setEntityId(entityFromMetisResponse.getEntityId());
+        entityRecord10.setEntityId("http://data.europeana.eu/timespan/16");
         entityRecordService.saveEntityRecord(entityRecord10);
 
         // perform referential integrity processing for da vinci record
