1a2b15fa884589a995ded69d013aa07136ba4b9c
Immanuel Kattey <immanuel.kattey@europeana.eu>
2021-12-15 10:03:57 +0100

    EA-2765: Fix empty jar error during mvn deploy

---
+++ entity-management-tests/src/main/java/eu/europeana/entitymanagement/TestNoopClass.java
@@ -0,0 +1,7 @@
+package eu.europeana.entitymanagement;
+
+/**
+ * Noop class to prevent Jar Plugin error during mvn deploy. see:
+ * https://stackoverflow.com/a/54673725/14530159
+ */
+public class TestNoopClass {}
