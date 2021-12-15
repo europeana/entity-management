89e8462cd4f096fa31e406855ecb8af457dbfbf4
Immanuel Kattey <immanuel.kattey@europeana.eu>
2021-12-15 10:12:55 +0100

    EA-2765: Fix SonarQube issue
    - change noop Class to Interface

--- entity-management-tests/src/main/java/eu/europeana/entitymanagement/TestNoopClass.java
+++ entity-management-tests/src/main/java/eu/europeana/entitymanagement/TestNoopClass.java
@@ -4,4 +4,4 @@ package eu.europeana.entitymanagement;
  * Noop class to prevent Jar Plugin error during mvn deploy. see:
  * https://stackoverflow.com/a/54673725/14530159
  */
-public class TestNoopClass {}
+public interface TestNoopClass {}
