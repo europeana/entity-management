6ac07187849a617a7c44218db4b83d792d67a3c8
Immanuel Kattey <immanuel.kattey@europeana.eu>
2022-02-25 15:23:05 +0100

    EA-2938: Fix typo

--- entity-management-definitions/src/main/java/eu/europeana/entitymanagement/serialization/StringOrListConverter.java
+++ entity-management-definitions/src/main/java/eu/europeana/entitymanagement/serialization/StringOrListConverter.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
  * Some Agent fields are serialized as String (in consolidated entity) or List (in proxy entities).
  * Within the Agent class, these fields are implemented as Lists.
  *
- * <p>This converter ensures properly deserialized.
+ * <p>This converter ensures proper serialization for the affected fields.
  */
 public class StringOrListConverter extends StdConverter<Object, List<String>> {
 
