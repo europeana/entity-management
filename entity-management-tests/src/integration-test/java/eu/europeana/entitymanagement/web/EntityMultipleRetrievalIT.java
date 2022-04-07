fe0e03dda3b9329dc41a663572471f0dc77d4d4c
Immanuel Kattey <immanuel.kattey@europeana.eu>
2022-04-07 12:53:31 +0100

    EA-2890: Improve test coverage

--- entity-management-tests/src/integration-test/java/eu/europeana/entitymanagement/web/EntityMultipleRetrievalIT.java
+++ entity-management-tests/src/integration-test/java/eu/europeana/entitymanagement/web/EntityMultipleRetrievalIT.java
@@ -5,11 +5,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import eu.europeana.entitymanagement.testutils.IntegrationTestUtils;
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -20,37 +17,28 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 public class EntityMultipleRetrievalIT extends BaseWebControllerTest {
 
   private static final String requestPath = "retrieve";
-  private static List<String> entityIds = new ArrayList<>();
 
-  @BeforeAll
-  public void setup() throws Exception {
+  @Test
+  public void multipleEntitiesRetrievedSuccessfully() throws Exception {
     // create 1st entity
     String europeanaMetadata = loadFile(IntegrationTestUtils.CONCEPT_REGISTER_BATHTUB_JSON);
     String metisResponse = loadFile(IntegrationTestUtils.CONCEPT_BATHTUB_XML);
-    String entityId =
+    String entityId1 =
         createEntity(europeanaMetadata, metisResponse, IntegrationTestUtils.CONCEPT_BATHTUB_URI)
             .getEntityId();
-    entityIds.add(entityId);
 
     // add second entity
     europeanaMetadata = loadFile(IntegrationTestUtils.AGENT_REGISTER_DAVINCI_JSON);
     metisResponse = loadFile(IntegrationTestUtils.AGENT_DA_VINCI_XML);
-    entityId =
+    String entityId2 =
         createEntity(europeanaMetadata, metisResponse, IntegrationTestUtils.AGENT_DA_VINCI_URI)
             .getEntityId();
-    entityIds.add(entityId);
 
-    Assertions.assertNotNull(entityIds);
-    Assertions.assertEquals(2, entityIds.size());
-  }
-
-  @Test
-  public void multipleEntitiesRetrievedSuccessfully() throws Exception {
     mockMvc
         .perform(
             MockMvcRequestBuilders.post(IntegrationTestUtils.BASE_SERVICE_URL + "/" + requestPath)
                 .accept(MediaType.APPLICATION_JSON)
-                .content(String.valueOf(entityIds)))
+                .content(String.valueOf(List.of(entityId1, entityId2))))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.entities", hasSize(2)));
   }
@@ -61,7 +49,30 @@ public class EntityMultipleRetrievalIT extends BaseWebControllerTest {
         .perform(
             MockMvcRequestBuilders.post(IntegrationTestUtils.BASE_SERVICE_URL + "/" + requestPath)
                 .accept(MediaType.APPLICATION_JSON)
-                .content("[ \"xyz\" , \"test\"]"))
+                // db is cleared between test runs, so these shouldn't exist
+                .content(
+                    "[ \"http://data.europeana.eu/concept/1\" , \"http://data.europeana.eu/concept/2\"]"))
         .andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void shouldReturnEntitiesWhenSomeInvalidEntitiesInRequest() throws Exception {
+    // create 1st entity
+    String europeanaMetadata = loadFile(IntegrationTestUtils.CONCEPT_REGISTER_BATHTUB_JSON);
+    String metisResponse = loadFile(IntegrationTestUtils.CONCEPT_BATHTUB_XML);
+    String entityId1 =
+        createEntity(europeanaMetadata, metisResponse, IntegrationTestUtils.CONCEPT_BATHTUB_URI)
+            .getEntityId();
+
+    // one invalid and valid entityId each
+    List<String> requestBody = List.of("http://data.europeana.eu/concept/0", entityId1);
+
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.post(IntegrationTestUtils.BASE_SERVICE_URL + "/" + requestPath)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(String.valueOf(requestBody)))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.entities", hasSize(1)));
   }
 }
