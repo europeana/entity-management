7cb2f5798ff8b840ee43c2b59237e8e7a80eb707
Srdjan Stevanetic <srdjan.stevanetic@gmail.com>
2022-06-21 10:41:40 +0200

    reverted back the use of SolrService instead of EntityRecordService in the EntitySolrRemovalWriter

--- entity-management-web/src/main/java/eu/europeana/entitymanagement/batch/writer/EntitySolrRemovalWriter.java
+++ entity-management-web/src/main/java/eu/europeana/entitymanagement/batch/writer/EntitySolrRemovalWriter.java
@@ -2,7 +2,7 @@ package eu.europeana.entitymanagement.batch.writer;
 
 import eu.europeana.entitymanagement.batch.utils.BatchUtils;
 import eu.europeana.entitymanagement.definitions.model.EntityRecord;
-import eu.europeana.entitymanagement.web.service.EntityRecordService;
+import eu.europeana.entitymanagement.solr.service.SolrService;
 import java.util.List;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.lang.NonNull;
@@ -11,15 +11,15 @@ import org.springframework.stereotype.Component;
 /** ItemWriter for removing entities from Solr */
 @Component
 public class EntitySolrRemovalWriter implements ItemWriter<EntityRecord> {
-  private final EntityRecordService entityRecordService;
+  private final SolrService solrService;
 
-  public EntitySolrRemovalWriter(EntityRecordService entityRecordService) {
-    this.entityRecordService = entityRecordService;
+  public EntitySolrRemovalWriter(SolrService solrService) {
+    this.solrService = solrService;
   }
 
   @Override
   public void write(@NonNull List<? extends EntityRecord> entityRecords) throws Exception {
     String[] entityIds = BatchUtils.getEntityIds(entityRecords);
-    entityRecordService.deleteFromSolr(List.of(entityIds));
+    solrService.deleteById(List.of(entityIds));
   }
 }
