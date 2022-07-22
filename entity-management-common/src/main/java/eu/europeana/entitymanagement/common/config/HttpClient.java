8c5d360e6f35427f8088da745a4b9b152e3b45bd
Srdjan Stevanetic <srdjan.stevanetic@gmail.com>
2022-07-22 18:17:29 +0200

    small code update for the try-with-resources

--- entity-management-common/src/main/java/eu/europeana/entitymanagement/common/config/HttpClient.java
+++ entity-management-common/src/main/java/eu/europeana/entitymanagement/common/config/HttpClient.java
@@ -13,29 +13,31 @@ import org.apache.http.util.EntityUtils;
 public class HttpClient {
 
   public static String httpGetClient(String url, Map<String, String> headers, Map<String, String> params) throws Exception {
-    CloseableHttpClient httpClient = HttpClients.createDefault();
-    HttpGet httpGet = new HttpGet(url);
-    if(headers!=null) {
-      for(Map.Entry<String, String> entry : headers.entrySet()) {
-        httpGet.addHeader(entry.getKey(), entry.getValue());
+    try(CloseableHttpClient httpClient = HttpClients.createDefault()) {
+      HttpGet httpGet = new HttpGet(url);
+      if(headers!=null) {
+        for(Map.Entry<String, String> entry : headers.entrySet()) {
+          httpGet.addHeader(entry.getKey(), entry.getValue());
+        }
       }
-    }
-    
-    URIBuilder uriBuilder = new URIBuilder(httpGet.getURI());
-    if(params!=null) {
-      for(Map.Entry<String, String> entry : params.entrySet()) {
-        uriBuilder.addParameter(entry.getKey(), entry.getValue());
+      
+      URIBuilder uriBuilder = new URIBuilder(httpGet.getURI());
+      if(params!=null) {
+        for(Map.Entry<String, String> entry : params.entrySet()) {
+          uriBuilder.addParameter(entry.getKey(), entry.getValue());
+        }
+      }    
+      ((HttpRequestBase) httpGet).setURI(uriBuilder.build());
+      
+      try(CloseableHttpResponse response = httpClient.execute(httpGet)) {      
+        if (response.getStatusLine().getStatusCode() != 200) {
+          return null;
+        }
+        HttpEntity entity = response.getEntity();
+        if (entity != null) {
+          return EntityUtils.toString(entity);
+        }
       }
-    }    
-    ((HttpRequestBase) httpGet).setURI(uriBuilder.build());
-    
-    CloseableHttpResponse response = httpClient.execute(httpGet);
-    if (response.getStatusLine().getStatusCode() != 200) {
-      return null;
-    }
-    HttpEntity entity = response.getEntity();
-    if (entity != null) {
-      return EntityUtils.toString(entity);
     }
     return null;
 
