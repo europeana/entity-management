bca5caefb507a2db4581e8f0e8fb3f5742ba88f6
Patrick Ehlert <patrick.ehlert@europeana.eu>
2023-02-08 14:32:20 +0100

    EA-3349 Support connecting to Mongo using SSL with user provided trust store

--- entity-management-mongo/src/main/java/eu/europeana/entitymanagement/mongo/config/DataSourceConfig.java
+++ entity-management-mongo/src/main/java/eu/europeana/entitymanagement/mongo/config/DataSourceConfig.java
@@ -13,7 +13,16 @@ import eu.europeana.batch.entity.JobExecutionEntity;
 import eu.europeana.entitymanagement.common.vocabulary.AppConfigConstants;
 import eu.europeana.entitymanagement.definitions.batch.codec.ScheduledTaskTypeCodec;
 import eu.europeana.entitymanagement.definitions.batch.codec.ScheduledTaskTypeCodecProvider;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.util.concurrent.TimeUnit;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bson.codecs.configuration.CodecProvider;
@@ -46,6 +55,12 @@ public class DataSourceConfig {
   @Value("${mongo.batch.database}")
   private String batchDatabase;
 
+  @Value("${mongo.truststore:}")
+  private String truststorePath;
+
+  @Value("${mongo.truststore.pwd:}")
+  private String truststorePwd;
+
   @Bean
   public MongoClient mongoClient() {
     ConnectionString connectionString = new ConnectionString(hostUri);
@@ -63,6 +78,41 @@ public class DataSourceConfig {
     Block<ConnectionPoolSettings.Builder> connectionPoolSettingsBlockBuilder =
         (ConnectionPoolSettings.Builder builder) ->
             builder.maxConnectionIdleTime(mongoMaxIdleTimeMillisec, TimeUnit.MILLISECONDS);
+
+    try {
+      // build SSL context that uses separate truststore (needs to be copied and specified via
+      // mongo.truststore property)
+      if (!truststorePath.isBlank()) {
+        KeyStore ks = KeyStore.getInstance("JKS");
+        ks.load(new FileInputStream(truststorePath), truststorePwd.toCharArray());
+        LogManager.getLogger(DataSourceConfig.class)
+            .info("Read truststore file {}", truststorePath);
+
+        TrustManagerFactory trustFactory =
+            TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        trustFactory.init(ks);
+
+        SSLContext sslContext = SSLContext.getInstance("TLSv1.3");
+        sslContext.init(null, trustFactory.getTrustManagers(), null);
+        return MongoClients.create(
+            MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .applyToConnectionPoolSettings(connectionPoolSettingsBlockBuilder)
+                .codecRegistry(codecRegistry)
+                .applyToSslSettings(builder -> builder.enabled(true).context(sslContext))
+                .build());
+      }
+    } catch (IOException e) {
+      throw new RuntimeException("Error reading truststore file", e);
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException("Error loading TLS protocol", e);
+    } catch (KeyStoreException e) {
+      throw new RuntimeException("Error initializing truststore", e);
+    } catch (CertificateException e) {
+      throw new RuntimeException("Error loading truststore", e);
+    } catch (KeyManagementException e) {
+      throw new RuntimeException("Error initializing SSL context", e);
+    }
 
     return MongoClients.create(
         MongoClientSettings.builder()
