0a9e6da319acd6c67ed9eafe2e342b10ff3e8d96
GordeaS <GordeaS@N3DSS2334.D03.arc.local>
2024-03-28 18:29:46 +0100

    cherrypick changes from branch EA-3721

--- entity-management-definitions/src/main/java/eu/europeana/entitymanagement/definitions/model/Organization.java
+++ entity-management-definitions/src/main/java/eu/europeana/entitymanagement/definitions/model/Organization.java
@@ -77,6 +77,9 @@ public class Organization extends Entity {
   private String countryId;
   @Transient
   private Place country;
+  @Transient
+  private String countryISO;
+  
   
   private List<String> europeanaRoleIds;
   @Reference(lazy = true)
@@ -335,5 +338,14 @@ public class Organization extends Entity {
   @JsonSetter(EUROPEANA_ROLE)
   public void setEuropeanaRole(List<Vocabulary> europeanaRole) {
     this.europeanaRole = europeanaRole;
+  }
+
+  @JsonIgnore
+  public String getCountryISO() {
+    return countryISO;
+  }
+
+  public void setCountryISO(String countryISO) {
+    this.countryISO = countryISO;
   }
 }
