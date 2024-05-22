c7a29a11170158b5a24350574c95c69f81483e11
GordeaS <GordeaS@host.docker.internal>
2024-05-22 10:35:54 +0200

    disable RequestPathMethodService for ZohoSync #EA-3789

--- entity-management-web/src/main/java/eu/europeana/entitymanagement/web/service/RequestPathMethodService.java
+++ entity-management-web/src/main/java/eu/europeana/entitymanagement/web/service/RequestPathMethodService.java
@@ -1,13 +1,14 @@
 package eu.europeana.entitymanagement.web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.WebApplicationContext;
 import eu.europeana.api.commons.web.service.AbstractRequestPathMethodService;
 
 /** This service is used to populate the Allow header in API responses. */
 @Service(value = "requestPathMethodService")
-//@ConditionalOnWebApplication
+@ConditionalOnWebApplication
 public class RequestPathMethodService extends AbstractRequestPathMethodService {
 
   @Autowired
