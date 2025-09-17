4a56a2969c2e4bd024dcfba1ea5da04927267101
srishti <srishti.singh@europeana.eu>
2025-09-17 20:52:28 +0200

    EA-4296 fix merge for deprecation IT

--- entity-management-tests/src/integration-test/java/eu/europeana/entitymanagement/web/EntityDeprecationIT.java
+++ entity-management-tests/src/integration-test/java/eu/europeana/entitymanagement/web/EntityDeprecationIT.java
@@ -40,30 +40,6 @@ public class EntityDeprecationIT extends BaseWebControllerTest {
                 .accept(MediaType.APPLICATION_JSON))
         .andExpect(status().isNoContent());
 
-    assertedTaskScheduled(entityRecord.getEntityId(), TaskType.deprecation);
-  }
-
-  @Test
-  void deprecationWithSyncProfileShouldBeSuccessful() throws Exception {
-    String europeanaMetadata = loadFile(IntegrationTestUtils.CONCEPT_REGISTER_BATHTUB_JSON);
-    String metisResponse = loadFile(IntegrationTestUtils.CONCEPT_BATHTUB_XML);
-
-    EntityRecord entityRecord =
-        createEntity(europeanaMetadata, metisResponse, IntegrationTestUtils.CONCEPT_BATHTUB_URI);
-
-    // confirm that Solr document is saved
-    SolrConcept solrConcept = solrService.searchById(SolrConcept.class, entityRecord.getEntityId());
-    Assertions.assertNotNull(solrConcept);
-
-    String requestPath = getEntityRequestPath(entityRecord.getEntityId());
-
-    mockMvc
-        .perform(
-            delete(IntegrationTestUtils.BASE_SERVICE_URL + "/" + requestPath)
-                .param(QUERY_PARAM_PROFILE, PARAM_PROFILE_SYNC)
-                .accept(MediaType.APPLICATION_JSON))
-        .andExpect(status().isNoContent());
-
     EntityRecord dbRecord = entityRecordService.retrieveEntityRecord(entityRecord.getEntityId(), EntityProfile.internal.name(), true); 
     Assertions.assertNotNull(dbRecord);
     Assertions.assertTrue(dbRecord.isDisabled());
