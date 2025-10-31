cb5a9118041fd32b551d0b7563047b6fe10d33cc
srishti <srishti.singh@europeana.eu>
2025-10-31 16:41:30 +0100

    Ea-4322 add enrich

--- entity-management-web/src/main/java/eu/europeana/entitymanagement/web/EMController.java
+++ entity-management-web/src/main/java/eu/europeana/entitymanagement/web/EMController.java
@@ -112,12 +112,12 @@ public class EMController extends BaseRest {
   }
 
   /**
-   * Endpoint to make sure we return a 400 for invalid urls like '/entity/0000', '/entity/0000/', "/entity/0000/management/"
+   * Endpoint to make sure we return a 400 for invalid urls like '/entity/0000', '/entity/0000/', "/entity/0000/management/enrich"
    * @throws HttpBadRequestException always for this endpoint
    * @return 400 bad Request
    */
   @SuppressWarnings("java:S6856") // no point in binding {any} to a variable
-  @PostMapping(value = {"/entity/{any}", "/entity/{any}/", "/entity/{any}/management/"})
+  @PostMapping(value = {"/entity/{any}", "/entity/{any}/", "/entity/{any}/management/enrich"})
   public ResponseEntity<String> entityManagementInvalidUrl() throws EuropeanaApiException {
     throw new HttpBadRequestException("Invalid Url");
   }
