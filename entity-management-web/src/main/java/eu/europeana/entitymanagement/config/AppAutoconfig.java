740884c1ffad6cb211c571f0cf9c83a563a0d702
srishti <srishti.singh@europeana.eu>
2025-11-12 14:57:14 +0100

    Ea-4326 use commons part

--- entity-management-web/src/main/java/eu/europeana/entitymanagement/config/AppAutoconfig.java
+++ entity-management-web/src/main/java/eu/europeana/entitymanagement/config/AppAutoconfig.java
@@ -161,7 +161,7 @@ public class AppAutoconfig extends AppConfigConstants {
     clientDetails.setApiKeyServiceUrl(emConfiguration.getApiKeyUrl());
     // Set authentication handler if values are not empty
     if (StringUtils.isNotEmpty(emConfiguration.getTokenEndpoint()) && StringUtils.isNotEmpty(emConfiguration.getGrantParams())) {
-      AuthenticationConfig config = new AuthenticationConfig(loadProperties());
+      AuthenticationConfig config = new AuthenticationConfig(emConfiguration.getTokenEndpoint(), emConfiguration.getGrantParams());
       clientDetails.setAuthHandler(AuthenticationBuilder.newAuthentication(config));
     } else {
       LOG.error("Keycloak token endpoint and parameters NOT set !!");
@@ -303,14 +303,6 @@ public class AppAutoconfig extends AppConfigConstants {
             new SynchronizedItemStreamReader<>();
     synchronizedItemStreamReader.setDelegate(reader);
     return synchronizedItemStreamReader;
-  }
-
-
-  private Properties loadProperties() {
-    Properties properties = new Properties();
-    properties.setProperty(AuthenticationConfig.CONFIG_TOKEN_ENDPOINT,emConfiguration.getTokenEndpoint());
-    properties.setProperty(AuthenticationConfig.CONFIG_GRANT_PARAMS,emConfiguration.getGrantParams());
-    return properties;
   }
 
 }
