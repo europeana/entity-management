f104523a6654b95aec67e0e38d50120579f83f59
SristiSingh-eu <srishti.singh@europeana.eu>
2026-07-24 11:21:32 +0200

    EA-4587 add Country field

--- entity-management-zoho/src/main/java/eu/europeana/entitymanagement/zoho/utils/ZohoConstants.java
+++ entity-management-zoho/src/main/java/eu/europeana/entitymanagement/zoho/utils/ZohoConstants.java
@@ -46,7 +46,8 @@ public final class ZohoConstants {
   public static final String SAME_AS_FIELD = "SameAs";
   public static final String STREET_FIELD = "Street";
   public static final String CITY_FIELD = "City";
-  public static final String COUNTRY_FIELD = "Organisation_Country";
+  public static final String COUNTRY_FIELD = "Country";
+//  public static final String COUNTRY_FIELD = "Organisation_Country";
   public static final String COUNTRY_URI_FIELD = "Country_URI";
 
   public static final String ZIP_CODE_FIELD = "Zip_code";
